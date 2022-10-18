536ff22b9d3ffb116633b71004252511fe4af451
AHSHA <ahsha98@gmail.com>
2022-10-18 21:38:27 +0800

    Add files via upload

---
+++ Scripts/smqnw_AS.js
@@ -0,0 +1,39 @@
+/***2022.1018
+脚本功能：扫描全能王-手机扫描仪+解锁VIP
+使用声明：此脚本仅供学习、测试与交流！
+
+[rewrite_local]
+
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/AHSHA98/QuanX/main/Scripts/smqnw_AS.js
+
+[mitm] 
+
+hostname = ap*.intsig.net
+
+*******************************/
+
+let body = JSON.parse($response.body);
+body.data.psnl_vip_property = {
+    "renew_method": "appstore",
+    "initial_tm": "1614867690",
+    "svip": 1,
+    "auto_renewal": true,
+    "ms_first_pay": 0,
+    "pending": 0,
+    "group2_paid": 0,
+    "inherited_flag": 0,
+    "nxt_renew_tm": "9915126887",
+    "level_info": {
+        "level": 1,
+        "days": 1,
+        "end_days": 30
+    },
+    "group1_paid": 1,
+    "ys_first_pay": 0,
+    "renew_type": "year",
+    "expiry": 8487890487,
+    "grade": 2,
+    "last_payment_method": "appstore",
+    "product_id": "com.intsig.camscanner.premiums.oneyear.autorenewable.svip.low"
+};
+$done({ "body": JSON.stringify(body) });
