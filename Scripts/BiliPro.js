ff22f1ddf1f3b8025facdc904b997ab9b3fab6fb
AHSHA <ahsha98@gmail.com>
2022-12-10 19:41:59 +0800

    Add files via upload

---
+++ Scripts/BiliPro.js
@@ -0,0 +1,36 @@
+/***2022.1210
+脚本功能：B大会员通用破解模板
+使用声明：此脚本仅供学习、测试与交流！
+
+*******************************
+
+
+[rewrite_local]
+  
+# ～ Bilibili大会员（2022-12-05）@ddgksf2013
+^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/AHSHA98/QuanX/main/Scripts/BiliPro.js
+
+
+[mitm] 
+
+hostname=app.bilibili.com, grpc.biliapi.net,*.biliapi.net,app.bilibili.com,api.bilibili.com,api.live.bilibili.com,api.vc.bilibili.com,dataflow.biliapi.com,124.239.240.*,101.89.57.66, 218.94.210.66,240e:b1:9801:206:11:0:0:*
+
+***********************************/
+
+ 
+
+
+var modifiedHeaders = $request['headers'];
+modifiedHeaders['Cookie'] = '';
+modifiedHeaders['x-bili-device-bin'] = '';
+modifiedHeaders['Authorization'] = '';
+modifiedHeaders['User-Agent'] = '';
+modifiedHeaders['buvid'] = '';
+modifiedHeaders['x-bili-metadata-bin'] = '';
+modifiedHeaders['x-bili-locale-bin'] = '';
+modifiedHeaders['x-bili-network-bin'] = '';
+modifiedHeaders['x-bili-fawkes-req-bin'] = '';
+modifiedHeaders['x-bili-trace-id'] = '';
+modifiedHeaders['x-bili-exps-bin'] = '';
+modifiedHeaders['x-bili-network-bin'] = '';
+$done({'headers': modifiedHeaders});
